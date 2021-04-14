0fd20d59bb8464967c0d18e69c7cdbd993773eb2
Yimning <1148967988@qq.com>
2021-04-14 19:09:23 +0800

    Initialize OldFiles

---
+++ src/test/java/com/yimning/YimningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yimning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YimningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
